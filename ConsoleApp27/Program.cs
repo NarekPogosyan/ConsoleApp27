6326c6bdb1e63e0c8cbf77348409f3df5f72c367
poghosyan karo <narekpoghosyan34@gmail.com>
2022-08-27 23:16:58 +0400

    Add project files.

---
+++ ConsoleApp27/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Fibonachii series by recursia ");
+Console.WriteLine("pliese insert matrixss size ");
+
+Console.WriteLine("n = ");
+int n = int.Parse(Console.ReadLine());
+Console.WriteLine("Fibonachii series " + Fibonachii(n));
+static int Fibonachii(int n)
+{
+    if (n == 0)
+        return 0;
+    if (n == 1)
+        return 1;
+
+    return (Fibonachii(n - 2) + Fibonachii(n - 1));
+}
